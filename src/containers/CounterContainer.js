a41ec9d890f314f5fff35df5835aed259a802697
Namju <skawn11114@gmail.com>
2021-08-10 12:01:25 +0900

    study. useSelector 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Counter from "../componenets/Counter";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { decrease, increase, setDiff } from "../modules/counter";
 
 /* store.getState() => useSelector의 state
@@ -10,11 +10,14 @@ import { decrease, increase, setDiff } from "../modules/counter";
 	}
 */
 export default function CounterContainer() {
-  //useSelector 상태를 조회하는 훅
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  //useSelector 상태를 조회하는 훅_ 현재 계속 새로운 객체를 생성하여 불러오는 형태(매번 렌더링이 발생함)
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual //equalityFn (prev, next비교) 최적화-1
+  ); //useSelector를 여러개 분리하여 선언 최적화-2
 
   //action을 생성하고__increase(), action을 발생__dispatch!
   const dispatch = useDispatch();
